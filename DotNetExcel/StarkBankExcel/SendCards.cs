18429d730d5c04e555aa8cc6237154997a31a686
luis.almeida <luis.almeida@starkbank.com>
2024-03-18 14:47:58 -0300

    Send Cards

---
+++ DotNetExcel/StarkBankExcel/SendCards.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace StarkBankExcel
+{
+    public partial class SendCards
+    {
+        private void Planilha18_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void Planilha18_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Código gerado pelo Designer VSTO
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(Planilha18_Startup);
+            this.Shutdown += new System.EventHandler(Planilha18_Shutdown);
+        }
+
+        #endregion
+
+    }
+}
