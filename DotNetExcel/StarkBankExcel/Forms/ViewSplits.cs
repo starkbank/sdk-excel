ae07eefe90d11491d12f169fd68fd9ee1f8304a7
luis.almeida <luis.almeida@starkbank.com>
2024-05-02 20:01:55 -0300

    Add date range selector

---
+++ DotNetExcel/StarkBankExcel/Forms/ViewSplits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StarkBankExcel.Forms
+{
+    public partial class ViewSplits : Form
+    {
+        public ViewSplits()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DotNetExcel/StarkBankExcel/GetSplit.cs
+++ DotNetExcel/StarkBankExcel/GetSplit.cs
@@ -10,6 +10,7 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using StarkBankExcel.Resources;
+using StarkBankExcel.Forms;
 
 namespace StarkBankExcel
 {
@@ -44,85 +45,8 @@ namespace StarkBankExcel
 
         private void button3_Click(object sender, EventArgs e)
         {
-            var worksheet = Globals.GetSplit;
-
-            int lastRow = worksheet.Cells[worksheet.Rows.Count, "A"].End[XlDirection.xlUp].Row;
-            Range range = worksheet.Range["A" + TableFormat.HeaderRow + ":V" + lastRow];
-            range.ClearContents();
-
-            worksheet.Range["A" + TableFormat.HeaderRow].Value = "Data";
-            worksheet.Range["B" + TableFormat.HeaderRow].Value = "ID da Invoice";
-            worksheet.Range["C" + TableFormat.HeaderRow].Value = "Seller";
-            worksheet.Range["D" + TableFormat.HeaderRow].Value = "CPF/CNPJ";
-            worksheet.Range["E" + TableFormat.HeaderRow].Value = "Valor";
-            worksheet.Range["F" + TableFormat.HeaderRow].Value = "Status";
-
-            Dictionary<string, object> optionalParam = new Dictionary<string, object>();
-
-            int row = TableFormat.HeaderRow + 1;
-
-            string cursor = "";
-            Dictionary<string, object> returnedData = new Dictionary<string, object>();
-
-            do
-            {
-                Dictionary<string, string> receiversById = new Dictionary<string, string>();
-                List<string> receiversList = new List<string>();
-                int logRow = row;
-
-                JObject respJson;
-
-                try
-                {
-                    respJson = SplitReceiver.Get(cursor);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-                if ((string)respJson["cursor"] != "") cursor = (string)respJson["cursor"];
-
-                JArray receivers = (JArray)respJson["receivers"];
-
-                foreach (JObject receiver in receivers)
-                {
-                    receiversById.Add(receiver["id"].ToString(), receiver["taxId"].ToString());
-                    receiversList.Add(receiver["id"].ToString());
-                }
-
-                try
-                {
-                    string receiversString = string.Join(",", receiversList);
-                    respJson = Split.Get("", new Dictionary<string, object>() { { "receiverIds", receiversString } });
-
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-                receivers = (JArray)respJson["splits"];
-
-                List<string> checkReceiverList = new List<string>();
-                Dictionary<string, Dictionary<string, int>> directionIds = new Dictionary<string, Dictionary<string, int>>();
-                int column = 7;
-
-                foreach (JObject receiver in receivers)
-                {
-                    worksheet.Range["A" + row].Value = receiver["created"].ToString();
-                    worksheet.Range["B" + row].Value = receiver["source"].ToString().Substring(8);
-                    worksheet.Range["C" + row].Value = receiver["receiverId"].ToString();
-                    worksheet.Range["D" + row].Value = receiversById[receiver["receiverId"].ToString()].ToString();
-                    worksheet.Range["E" + row].Value = double.Parse((string)receiver["amount"].ToString()) / 100;
-                    worksheet.Range["F" + row].Value = receiver["status"].ToString();
-
-                    row++;
-                }
-
-            } while (cursor != null);
+            ViewSplit viewSplit = new ViewSplit();
+            viewSplit.ShowDialog();
         }
 
         private void button1_Click(object sender, EventArgs e)
