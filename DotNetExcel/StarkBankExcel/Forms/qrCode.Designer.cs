0aff84794ed8c8bacad81f986c21323c9e1adadb
luis.almeida <luis.almeida@starkbank.com>
2024-05-06 20:09:31 -0300

    wip

--- DotNetExcel/StarkBankExcel/Forms/qrCode.Designer.cs
+++ DotNetExcel/StarkBankExcel/Forms/qrCode.Designer.cs
@@ -52,6 +52,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(552, 222);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -60,21 +61,19 @@
             this.pictureBox2.Image = global::StarkBankExcel.Properties.Resources.modal_excel__4_;
             this.pictureBox2.Location = new System.Drawing.Point(-19, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(920, 520);
+            this.pictureBox2.Size = new System.Drawing.Size(920, 492);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // qrCode
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(904, 489);
             this.Controls.Add(this.emailLabel);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
             this.Name = "qrCode";
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dados de Acesso";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- DotNetExcel/StarkBankExcel/Forms/qrCode.cs
+++ DotNetExcel/StarkBankExcel/Forms/qrCode.cs
@@ -23,20 +23,37 @@ namespace StarkBankExcel.Forms
         {
            InitializeComponent();
 
-           string bString = Globals.Credentials.Range["B14"].Value;
-           
-           string email = Globals.Credentials.Range["B2"].Value;
+           string poar = Globals.Credentials.Range["C16"].Value;
 
-           string attachment = bString.Substring(bString.IndexOf("base64,") + "base64,".Length);
+           Dictionary<string, string> sizeValue = new Dictionary<string, string>();
 
-           byte[] attachmentb64 = Convert.FromBase64String(attachment);
+           sizeValue = stringToDict(poar);
 
-           byte[] resizedImageBytes = Resources.B64ToFile.ResizeImage(attachmentb64, 180, 180);
+            if (sizeValue != null)
+            {
+                Dictionary<string, int> qrCodeLocationValue = locationReturner(sizeValue, 1674, 2398);
 
-           Image originalImage = ByteArrayToImage(resizedImageBytes);
+                Dictionary<string, int> textLocationValue = locationReturner(sizeValue, 10880, 4567);
 
-           pictureBox1.Image = originalImage;
-           emailLabel.Text = email.ToString();
+                string bString = Globals.Credentials.Range["B14"].Value;
+
+                string email = Globals.Credentials.Range["B2"].Value;
+
+                string attachment = bString.Substring(bString.IndexOf("base64,") + "base64,".Length);
+
+                byte[] attachmentb64 = Convert.FromBase64String(attachment);
+
+                byte[] resizedImageBytes = Resources.B64ToFile.ResizeImage(attachmentb64, 180, 180);
+
+                Image originalImage = ByteArrayToImage(resizedImageBytes);
+
+                pictureBox1.Image = originalImage;
+                pictureBox2.Size = new Size(Int32.Parse(sizeValue["Width"]), Int32.Parse(sizeValue["Height"]));
+                pictureBox1.Location = new System.Drawing.Point(qrCodeLocationValue["x"], qrCodeLocationValue["y"]);
+                emailLabel.Location = new System.Drawing.Point(textLocationValue["x"], textLocationValue["y"]);
+
+                emailLabel.Text = email.ToString();
+            }
         }
 
         static Image ByteArrayToImage(byte[] byteArrayIn)
@@ -46,6 +63,52 @@ namespace StarkBankExcel.Forms
                 Image image = Image.FromStream(ms);
                 return image;
             }
+        }
+
+        static Dictionary<string, int> locationReturner(Dictionary<string, string> inputLocation, int mxValue, int myValue)
+        {
+
+            int delta = 1000;
+
+            int mx = mxValue;
+            int my = myValue;
+
+            // int mx = 1674;
+            // int my = 2398;
+
+            int x = Int32.Parse(inputLocation["Width"]) * delta;
+            int y = Int32.Parse(inputLocation["Height"]) * delta;
+
+            x = (x / mx);
+            y = (y / my);
+
+            Dictionary<string, int> locationDict = new Dictionary<string, int>()
+            {
+                { "x", x },
+                { "y", y },
+            };
+
+            return locationDict;
+        }
+
+        static Dictionary<string, string> stringToDict(string input)
+        {
+            if (input == null) { return null; };
+
+            input = input.Replace("{", "");
+            input = input.Replace("}", "");
+
+            string[] inputs = input.Split(',');
+
+            Dictionary<string, string> myDict = new Dictionary<string, string>();
+
+            foreach (string c in inputs)
+            {
+                string[] elements = c.Split('=');
+                myDict[elements[0].Trim()] = elements[1].Trim();
+            }
+
+            return myDict;
         }
     }
 }
